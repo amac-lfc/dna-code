d44935570482c23a64dbc667f415d5399bf40ff8
MrPowerpalm <fennnapier@gmail.com>
2019-05-20 10:07:04 -0500

    added a program to merge the computed CSV where gene name match and genrate a cancer methelations status compaired to normal, also added the resulting csv

---
+++ MethylMixCode/UpDownReg.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+#reading the CVS Files
+norm = pd.read_csv('computed_METnormal_OV.csv', delimiter="\t", \
+                    names = ["gene", "Nmean", "NSD"])
+cancer = pd.read_csv('computed_METcancer_OV.csv', delimiter="\t", \
+                    names = ["gene", "Cmean", "CSD"])
+
+mergedDF = pd.merge(norm, cancer, on='gene')
+
+def condition(row):
+    if row['Nmean'] == row['Cmean']:
+        val= "NoChange"
+    elif row['Nmean'] > row['Cmean']:
+        val = "Down"
+    else:
+        val = "Up"
+    return val
+
+mergedDF['methelation'] = mergedDF.apply(condition, axis=1)
+
+mergedDF.to_csv("mergedMethylStatus.csv")
