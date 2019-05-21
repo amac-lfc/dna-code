897f8727ce13ef04bb8855cd48c30027400809fa
MrPowerpalm <fennnapier@gmail.com>
2019-05-21 14:36:19 -0500

    added the x matrix pkl and updated the cluster reader file which i really should rename to uniform collum and pkl generator thing... name in the works

--- MethylMixCode/clusterReader.py
+++ MethylMixCode/clusterReader.py
@@ -4,12 +4,22 @@ import numpy as np
 MET =  pd.read_csv("METcancer_OV.csv", delimiter = ",")
 GE  = pd.read_csv("GEcancer_OV.csv", delimiter = ",")
 
-METp = list(MET)
-GEp = list(GE)
+METp = list(MET.columns.values)
+GEp = list(GE.columns.values)
 
 geneList = GE["Unnamed: 0"].values
-inBoth = set(METp).intersection(GEp)
+inBoth = set(GEp).intersection(METp)
 
-GE.loc[inBoth]
+inBoth = list(inBoth)
+GE = GE[inBoth]
 
-print(MET.shapes)
+MET = MET[inBoth]
+
+
+MET = MET.reindex(sorted(MET.columns), axis=1)
+GE = GE.reindex(sorted(GE.columns), axis=1)
+
+MET.to_csv("MatrixX.csv")
+GE.to_csv("MatrixY.csv")
+# print(MET)
+# print(GE)
