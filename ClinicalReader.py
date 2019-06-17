40e14f259679590aaee62ddc1d33cc22e0e5c4f1
MrPowerpalm <fennnapier@gmail.com>
2019-06-17 16:02:37 -0500

    debugging stage

--- ClinicalReader.py
+++ ClinicalReader.py
@@ -12,16 +12,29 @@ Norm = pd.read_csv("../../OVCancerDataSet/GEnormal_OV.csv", \
                     delimiter = ",")
 Norm = Norm.rename(columns = {"Unnamed: 0": "genes"})
 
+Debug = True
+
+temp = np.where(pd.isnull(Clin[736])) 
+Clin = np.delete(Clin, [temp], axis =1)
+
+
+if Debug == True:
+        print("CLinD shape 18", ClinD.shape)
+        print("exp shape", Exp.shape)
+        print("Norm shape",Norm.shape)
 
 #get only genes in both Expresion and Norm
 G = Exp["Hybridization REF"].values[1:]
 G = G.astype(str)
 G = [x for x in G if x in Norm['genes'].values]
+if Debug: print("shape of G line 16", len(G))
 
 # Patient ID dictionary
 Cpat = Clin[17]
 for i in range(len(Cpat)):
     Cpat[i] = Cpat[i].upper()
+
+if Debug: print("Clin patients", Cpat.shape)
 
 Etemp = Exp.columns
 Epat = np.zeros(0, dtype = str)
@@ -32,20 +45,26 @@ for i in range(len(Etemp)):
     dicts.update({temp[:12] : Etemp[i]})
     Epat = np.append(Epat, temp[:12])
 
+if Debug: print("# of Exp patients", Epat.shape)
+
 B = np.intersect1d(Epat, Cpat, return_indices = True)
 Bpat = B[0]
 patIndexExp = B[1] 
 patIndexClin = B[2]
 
+if Debug: print("List of both patients", Bpat.shape)
 
 # PatientXgene Matrix generation
 
 Exp = Exp[Exp["Hybridization REF"].isin(G)]
 
+if Debug: print("Exp after removal of non simialr genes", Exp.shape)
 
 #Interface Gen
 normGenes = Norm["genes"].values
 normGenes = normGenes.astype(str)
+
+if Debug: print("norm genes length", normGenes.shape )
 
 interface = np.zeros(len(Norm["genes"].values), "i")
 k=0
@@ -60,7 +79,12 @@ fullNames = []
 for i in range(len(Bpat)):
         fullNames.append(dicts.get(Bpat[i]))
 fullNames = np.asarray(fullNames)
-Exptemp = Exp[fullNames]
+Exptemp = Exp[fullNames] 
+#not touched again untill the end when converted to Numpy matrix
+
+if Debug: print("shape of interface", interface.shape)
+
+if Debug: print("Exp after removal of no matching names", Exptemp.shape)
 
 # GEN gen
 GEN = np.zeros(0, dtype = float)
@@ -81,29 +105,45 @@ for i in range(len(interface)):
         GElist[interface[i]-1] = Norm.values[i,0]
 GE = np.transpose(GE)
 
-# Days to death
+# Y Gen
 CIndex = []
 for x in range(len(Exp.columns)):
     if x not in patIndexClin:
-        CIndex.append(x)
+        CIndex.append(x)   
+if Debug: print("number of items being deleted form both DtD and life"\
+                ,len(CIndex))
 DtD = np.delete(Clin[22], CIndex)
 life = np.delete(Clin[736], CIndex)
 
-temp = []
+if Debug == True:
+        print("number of patients in alive", life.shape)
+        print("number of patients with days to death", DtD.shape)
+
+tempL = []
 for i in range(len(DtD)):
         if str(DtD[i]) == "nan" and str(life[i]) == "alive":
                 DtD[i] = -1
-        if str(DtD[i]) == "nan":
-                temp.append(i)
-DtD = np.delete(DtD, temp)
+        elif str(DtD[i]) == "nan":
+                tempL.append(i)
+
+DtD = np.delete(DtD, tempL)
+
+if Debug: print("patinets after merging DtD and life while removing NAN"\
+                , DtD.shape)    
 Y = DtD
 
 
 #convert newExp to X and make numpy matrix
 newExp = Exptemp.to_numpy()
+
+if Debug: print("newExp to numpy shape", newExp.shape)
 newExp = newExp.astype(np.float)
 newExp = np.transpose(newExp)
+
+if Debug: print("new Exp after transposition", newExp.shape)
 X = newExp
+
+if Debug: print("shape of X for good measure", X.shape)
 
 # printStatments
 np.save("FirePkl/y", Y, allow_pickle= True)
