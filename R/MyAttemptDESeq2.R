266e68d7c2868258101949ba13ff5c424b3f757c
MrPowerpalm <fennnapier@gmail.com>
2019-08-01 15:49:05 -0500

    end of day push

--- R/MyAttemptDESeq2.R
+++ R/MyAttemptDESeq2.R
@@ -3,7 +3,10 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 
 BiocManager::install("DESeq2")
 
-dds <- DESeqDataSetFromMatrix(countData = cts,
+HRNA <- read.csv('DataUsedforDESeq2/HNSC.rnaseq__illuminahiseq_rnaseq__unc_edu__Level_3__gene_expression__data.data.txt', header = TRUE, row.names = 1)
+HRNA_data <- data.matrix(HRNA)
+
+dds <- HRNA_data(countData = cts,
                               colData = coldata,
                               design= ~ batch + condition)
 dds <- DESeq(dds)
