0cb8f93347f82af321f2ab56edaf553a4cac1dce
MrPowerpalm <fennnapier@gmail.com>
2019-08-01 14:35:25 -0500

    I made this way to complicated and dont want to delete the origional because its basicaly the tutorial at this point

---
+++ R/MyAttemptDESeq2.R
@@ -0,0 +1,13 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("DESeq2")
+
+dds <- DESeqDataSetFromMatrix(countData = cts,
+                              colData = coldata,
+                              design= ~ batch + condition)
+dds <- DESeq(dds)
+resultsNames(dds) # lists the coefficients
+res <- results(dds, name="condition_trt_vs_untrt")
+# or to shrink log fold changes association with condition:
+res <- lfcShrink(dds, coef="condition_trt_vs_untrt", type="apeglm")
